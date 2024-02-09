38a7647a59f21b49f11ae4cc8d713cf84889d7e8
AdityaGaur7 <adgaur027@gmail.com>
2024-02-09 11:55:20 +0530

    frontend code

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+
+import { Component } from "@/components/component/component1";
+import { Component5 } from "@/components/component/component4";
+// import { Component3 } from "@/components/component/component3";
+// import { Component4 } from "@/components/component/component4";
+export default function Home() {
+  return (
+    <>
+    <Component/>
+    <Component5/>
+    {/* <Component3/> */}
+    {/* <Component4/> */}
+    </>
+  );
+}
